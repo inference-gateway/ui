f32a6a63fce0c42300aa5faef86fa6800c70234e
Eden Reich <eden.reich@gmail.com>
2025-03-28 16:24:34 +0000

    refactor: Standardize formatting in model.ts by adding semicolons
    Signed-off-by: Eden Reich <eden.reich@gmail.com>

--- types/model.ts
+++ types/model.ts
@@ -1,8 +1,7 @@
 export interface Model {
-  id: string
-  object: string
-  created: number
-  owned_by: string
-  served_by: string
+  id: string;
+  object: string;
+  created: number;
+  owned_by: string;
+  served_by: string;
 }
-
