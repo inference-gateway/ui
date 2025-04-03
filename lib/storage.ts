a1aa88a9f0edb28d4fe7939504181c6564db7f89
Eden Reich <eden.reich@gmail.com>
2025-04-03 19:48:27 +0000

    chore: Add TODO for additional storage type implementation
    Signed-off-by: Eden Reich <eden.reich@gmail.com>

--- lib/storage.ts
+++ lib/storage.ts
@@ -4,6 +4,8 @@ import { LocalStorageService } from "./storage-local";
 export class StorageServiceFactory {
   static createService(options?: StorageOptions): StorageService {
     switch (options?.storageType) {
+      // TODO - add at least one more storage type for external data persistence
+      // Check how is supabase storage implemented - probably a good option.
       default:
         return new LocalStorageService(options);
     }
